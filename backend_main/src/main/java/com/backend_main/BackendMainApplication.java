6a2a1cac3e262d63574885d64fa9576d194ef7a2
Hemanth Pitchika <hemanth.pitchika@ad.infosys.com>
2024-07-29 11:02:13 +0530

    Done Small modification

--- backend_main/src/main/java/com/backend_main/BackendMainApplication.java
+++ backend_main/src/main/java/com/backend_main/BackendMainApplication.java
@@ -13,7 +13,9 @@ public class BackendMainApplication {
 	public static void main(String[] args) {
 		Logger log = LogManager.getLogger(BackendMainApplication.class);
 		SpringApplication.run(BackendMainApplication.class, args);
+		System.out.println("Application Started");
 		log.info("Started Main Backend Application");
+		
 	}
 
 }
